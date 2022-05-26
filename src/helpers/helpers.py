078ccff506cf741ec466bc473d4fc910f55b3075
Egor Spirin <eg.spirin@gmail.com>
2022-05-26 21:03:37 +0200

    Prepare for getting the matrix from OSRM

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 
 
 def list_difference(list1: List, list2: List) -> List:
@@ -8,3 +8,17 @@ def list_difference(list1: List, list2: List) -> List:
             result.append(item)
 
     return result
+
+
+def format_matrix(sources: List[int], destinations: List[int], matrix: List[float]) -> Dict[str, Dict[str, int]]:
+    sources = [str(source) for source in sources]
+    destinations = [str(destination) for destination in destinations]
+    formatted_matrix = dict()
+    len_sources = len(sources)
+    len_destinations = len(destinations)
+    for i in range(len_sources):
+        formatted_matrix[sources[i]] = dict()
+        for j in range(len_destinations):
+            formatted_matrix[sources[i]][destinations[j]] = int(matrix[i * len_sources + j])
+
+    return formatted_matrix
