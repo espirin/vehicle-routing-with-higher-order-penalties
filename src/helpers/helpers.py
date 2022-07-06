4e8eb000b4936b24097ea5f6d398fe80af1017fb
Egor Spirin <eg.spirin@gmail.com>
2022-07-06 18:25:23 +0200

    Make prettier, refactor the notebook

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -1,5 +1,9 @@
 from typing import List, Dict
 
+from src.routing_problem.lanelet import Lanelet
+from src.routing_problem.maneuver.modifier import ManeuverModifier
+from src.routing_problem.routing_problem import RoutingProblem
+
 
 def list_difference(list1: List, list2: List) -> List:
     result = []
@@ -22,3 +26,37 @@ def format_matrix(sources: List[int], destinations: List[int], matrix: List[floa
             formatted_matrix[sources[i]][destinations[j]] = int(matrix[i * len_sources + j])
 
     return formatted_matrix
+
+
+def create_straights(order: List[Lanelet], rp: RoutingProblem) -> List[float]:
+    straights = []
+    previous_lanelet = None
+    current_straight = 0
+
+    for lanelet in order:
+        if previous_lanelet is None:
+            previous_lanelet = lanelet
+            continue
+
+        if (previous_lanelet.segment.id, lanelet.segment.id) in rp.maneuvers:
+            maneuver = rp.maneuvers[(previous_lanelet.segment.id, lanelet.segment.id)]
+
+            if maneuver.modifier == ManeuverModifier.Straight:
+                if current_straight == 0:
+                    current_straight = previous_lanelet.segment.get_length() + lanelet.segment.get_length()
+                else:
+                    current_straight += lanelet.get_length()
+            else:
+                if current_straight > 0:
+                    straights.append(current_straight)
+                    current_straight = 0
+        elif current_straight > 0:
+            straights.append(current_straight)
+            current_straight = 0
+
+        previous_lanelet = lanelet
+
+    if current_straight > 0:
+        straights.append(current_straight)
+
+    return straights
