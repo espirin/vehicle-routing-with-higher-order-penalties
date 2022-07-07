f253a55c0b8adf8c4aade2aee34e60bd1fc11b7f
Egor Spirin <eg.spirin@gmail.com>
2022-07-07 13:11:24 +0200

    Bug fixes

--- src/helpers/resources_monitor.py
+++ src/helpers/resources_monitor.py
@@ -33,16 +33,19 @@ def measure_resources_usage(function, args, return_dict: Dict = None, proc_num: 
         monitor = ResourcesMonitor()
         mem_thread = executor.submit(monitor.measure_usage)
         try:
-            fn_thread = executor.submit(function, args=args + (return_dict, proc_num))
+            args = args + (return_dict, proc_num)
+            fn_thread = executor.submit(function, *args)
             _ = fn_thread.result()
         finally:
             monitor.keep_measuring = False
             memory_usage, user_mode_time, system_mode_time = mem_thread.result()
 
         if return_dict is not None:
-            return_dict[proc_num]['memory'] = memory_usage
-            return_dict[proc_num]['cpu_user'] = user_mode_time
-            return_dict[proc_num]['cpu_system'] = system_mode_time
+            return_dict[proc_num + 100] = {
+                "memory": memory_usage,
+                "cpu_user": user_mode_time,
+                "cpu_system": system_mode_time,
+            }
         else:
             print(f"Memory usage: {memory_usage}")
             print(f"CPU user time: {user_mode_time}")
